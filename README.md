07bf99a91717d5082bd851dbd662df08626f6f2b
narutompel <putujackson0@gmail.com>
2024-04-06 11:19:34 +0800

    update tampilan sidebar berisikan list navigasi

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ A new Flutter project.
 
 Putu Adi Saputra
 42130086
+
+apps toko sepatu:
+- profile = sudah
+- tampilan sidebar = belum
+- implementasi dark mode atau light mode = belum
+- penambahan tampilan home = 
+- penambahan tampilan catalog = 
+- penambahan tampilan wishlist = 
+- penambahan tampilan pada halaman salah satu sepatu yang diklik pada halaman katalog = 
--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,62 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      drawer: _myDrawer(),
     );
   }
+}
+
+Widget _myDrawer() {
+  return SizedBox(
+    child: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const UserAccountsDrawerHeader(
+            currentAccountPicture: Image(
+              image: AssetImage('aset/images/Profile.png'),
+            ),
+            accountName: Text("Putu Adi Saputra"),
+            accountEmail: Text("putujackson0@gmail.com"),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.unsplash.com/photo-1533861225287-75902a93f681?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                    fit: BoxFit.cover)),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_2_sharp),
+            title: const Text("Profile"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_cart),
+            title: const Text("Catalog"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.favorite_sharp),
+            title: const Text("Wishlist"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("Home"),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("Settings"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Log out"),
+            onTap: () {},
+          ),
+        ],
+      ),
+    ),
+  );
 }
